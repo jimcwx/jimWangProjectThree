425309a460fb56a3d40547bd49b497df0400f69c
Jim Wang <jimcwx@gmail.com>
2020-02-09 14:20:43 -0500

    bare bone basics done

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ $(function() {
   //Declaring Variables
   
 
-  const player = {};
+  const player = {
+    health: 100,
+  };
+
+  const monster = {
+    health: 100,
+    name: "Donald",
+    weapon: {
+      name: "teeth",
+      damage: 2,
+      critical: 5,
+    }
+  }
 
   const weapons = [
     {
@@ -31,6 +43,11 @@ $(function() {
       }
     }
   }
+  //Random Interger generater between 0 to max where max is a parameter that will be filled with a number value
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
 
   //When the user clicks enter after entering their name, Everything in the below callback function happens.
   $("form.playerName").on("submit", function(event) {
@@ -43,6 +60,11 @@ $(function() {
   
       //This hides the form where the user entered their player name
       $(this).toggleClass("hideYesFootprint");
+
+      //Populating text tags for the name and health on the battle screen
+      $(".battle .hero h3").text(`${player.name}`);
+      $(".battle .monster h3").text(`${monster.name}`);
+
   
       //This adds the line asking the player to pick their weapon;
       $(".selectionMessage").text(`
@@ -58,8 +80,92 @@ $(function() {
 
   })
 
+  //This runs the above callback function when enter key is hit instead of a mouse click
+  $(".weaponSelection li").keydown(function (event) {
+    if (event.which === 13) {
+      $(this).click();
+    }
+  });
+
+  //A callback function that happens when a weapon is selected
+  $(".weaponSelection li").on("click", function(){
+    //adding the weapon object to the player object
+    //name of the weapon the player picked
+    const weaponName = $(this).attr("id");
+    const userConfirmation = confirm(`You are about to select the ${weaponName}, are you sure?`);
+    //Checking if the user is sure about their weapon choice to counter-act accidental clicks.
+    if (userConfirmation) {
+      //searching through the weapons array for the correct weapon to add to the player object
+      const playerWeapon = search(weaponName, weapons);
+      player.weapon = playerWeapon;
+
+      //populating the text tags in the battle section of the document
+      $(".selectionMessage").text(`
+        Well then ${player.name}, with your ${player.weapon.name} in hand it's time to start the battle!!
+      `);
+      $(".battle .hero p span").text(`${player.health}/100`);
+      $(".battle .monster p span").text(`${monster.health}/100`);
+      
+      //Hiding away the weapon selection and brings forth the battle screen
+      $(".weaponSelection").toggleClass("hideNoFootprint");
+      $(".battle").toggleClass("hideNoFootprint");
+      $('html,body').animate({ scrollTop: 9999 }, 'slow');
+    }
+
+
+  })
+
+  function letsBattle(playerObject, monsterObject) {
+    const playerCriticalFactor = getRandomInt(3);
+    const monsterCriticalFactor = getRandomInt(3);
+    let playerDamageToTake;
+    let monsterDamageToTake;
+    //the below code makes it so that whenever the random interger generates the interger 2, the critical property is used as the damage instead of the damage property. The overall resulting integers could only be either 0, 1, or 2 therefore the chance of hitting 2 is 1/3. I am also populating the p tags showing the damage events on the document.
+    if (playerCriticalFactor === 2) {
+      monsterDamageToTake = playerObject.weapon.critical;
+      $(".battle .events .heroAttack").text(`${playerObject.name} critically strikes ${monsterObject.name} for ${monsterDamageToTake} damage.`)
+    } else {
+      monsterDamageToTake = playerObject.weapon.damage;
+      $(".battle .events .heroAttack").text(`${playerObject.name} attacks ${monsterObject.name} for ${monsterDamageToTake} damage.`)
+    }
+    if (monsterCriticalFactor === 2) {
+      playerDamageToTake = monsterObject.weapon.critical;
+      $(".battle .events .monsterAttack").text(`${monsterObject.name} critically strikes ${playerObject.name} for ${playerDamageToTake} damage.`)
+    } else {
+      playerDamageToTake = monsterObject.weapon.damage;
+      $(".battle .events .monsterAttack").text(`${monsterObject.name} attacks ${playerObject.name} for ${playerDamageToTake} damage.`)
+    }
+    
+    //Below I will subtract the damage from the health for both battling objects and then update the health text on the document.
+    monsterObject.health = monsterObject.health - monsterDamageToTake;
+    playerObject.health = playerObject.health - playerDamageToTake;
+    $(".battle .hero p span").text(`${playerObject.health}/100`);
+    $(".battle .monster p span").text(`${monsterObject.health}/100`);
+
+    //Below we go to the end game screen when either the playerObject or the monsterObject reaches 0 health
+    if (playerObject.health <= 0) {
+      $(".battle").toggleClass("hideNoFootprint");
+      $(".endScreen.loserScreen").toggleClass("hideNoFootprint");
+    } else if (monsterObject.health <= 0) {
+      $(".battle").toggleClass("hideNoFootprint");
+      $(".endScreen.victoryScreen").toggleClass("hideNoFootprint");
+    }
+
+  }
+
   
-
-
+  
+  //When the user clicks or hits enter on the attack button, the player object will battle the monster object
+  $("form.attack").on("submit", function(event) {
+    event.preventDefault();
+    letsBattle(player, monster);
+    $('html,body').animate({ scrollTop: 9999 }, 'slow');
+  })
+  
+  
+  //When the user clicks play again the entire page will hard refresh
+  $("form.playAgain").on("submit", function(event) {
+    location.reload(true);
+  })
 
 })
