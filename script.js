c5bb9e418f83b74bb64b625974afc09b911a6dc4
Jim Wang <jimcwx@gmail.com>
2020-02-09 11:16:18 -0500

    added images of weapons

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ $(function() {
       damage: 3,
       critical: 8,
     }
-  ]
+  ];
 
 
   //This is a search function where the parameters will be a nameKey(string) and myArray(array) where it searches through the objects inside myArray and returns the object where the value of the name property matches the nameKey.
@@ -34,13 +34,31 @@ $(function() {
 
   //When the user clicks enter after entering their name, Everything in the below callback function happens.
   $("form.playerName").on("submit", function(event) {
-    event.preventDefault();
-    //Populating the player object with a name
-    player.name = $("input.playerName").val();
-    
-    $(this).toggleClass("hideYesFootprint")
-    
+
+    //this checks if the user entered anything;
+    if ($("input.playerName").val()) {
+      event.preventDefault();
+      //Populating the player object with a name
+      player.name = $("input.playerName").val();
+  
+      //This hides the form where the user entered their player name
+      $(this).toggleClass("hideYesFootprint");
+  
+      //This adds the line asking the player to pick their weapon;
+      $(".selectionMessage").text(`
+        Welcome ${player.name}, Please Select your weapon.
+      `);
+      
+      //This reveals the weaponSelection to the player
+      $(".weaponSelection").toggleClass("hideNoFootprint");
+      //This scrolls to the bottom of the page
+      $('html,body').animate({ scrollTop: 9999 }, 'slow');
+
+    }
+
   })
+
+  
 
 
 
